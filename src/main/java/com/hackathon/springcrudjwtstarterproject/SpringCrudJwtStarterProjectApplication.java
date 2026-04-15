f4b84a5a5f500249405a4795360bf2c5f206f9d0
Maxim Brett <maxim.brett.ru@gmail.com>
2026-04-16 00:12:13 +0400

    refactor: fix typo in base package name (hackaton -> hackathon)

--- src/main/java/com/hackathon/springcrudjwtstarterproject/SpringCrudJwtStarterProjectApplication.java
+++ src/main/java/com/hackathon/springcrudjwtstarterproject/SpringCrudJwtStarterProjectApplication.java
@@ -1,4 +1,4 @@
-package com.hackaton.springcrudjwtstarterproject;
+package com.hackathon.springcrudjwtstarterproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/hackathon/springcrudjwtstarterproject/SpringCrudJwtStarterProjectApplicationTests.java
+++ src/test/java/com/hackathon/springcrudjwtstarterproject/SpringCrudJwtStarterProjectApplicationTests.java
@@ -1,4 +1,4 @@
-package com.hackaton.springcrudjwtstarterproject;
+package com.hackathon.springcrudjwtstarterproject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
